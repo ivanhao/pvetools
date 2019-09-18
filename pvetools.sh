de15cc6388753f5f7f3f8cb5be9b00f26b85b8f4
ivanhao <ivanhao1984@qq.com>
2019-09-18 17:48:51 +0800

    1.0

--- pvetools.sh
+++ pvetools.sh
@@ -347,8 +347,7 @@ c )
         esac
     done
     if [ `grep "^\[${n}\]$" /etc/samba/smb.conf|wc -l` != 0 ];then
-
-        echo "Configed!"
+        sed '/\[${n}\]/,/0700/d' /etc/samba/smb.conf -i echo "Configed!"
         echo "配置成功!"
         service smbd restart
     fi
