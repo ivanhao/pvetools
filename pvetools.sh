7e1941f8a04dffe76992755ea70af22b0d9d7116
ivanhao <ivanhao1984@qq.com>
2019-09-19 16:34:08 +0800

    1.0

--- pvetools.sh
+++ pvetools.sh
@@ -148,10 +148,10 @@ if [ -f /etc/mailname ];then
 fi
 echo -e "\033[31mWill you want to config mailutils & postfix to send notification?(Y/N):\033[0m"
 echo -e "\033[31m是否配置mailutils和postfix来发送邮件通知?(Y/N):\033[0m"
-echo "$1: "$1
+echo "1: "$1
 if [ $1 ];then
     echo "c: "$c
-    if [ $c = "1" ];then
+    if [ $c == "1" ];then
         x=n
     else
         x=a
