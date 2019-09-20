e7756a84e356dd15727859e19d1f83413c180c87
ivanhao <ivanhao1984@qq.com>
2019-09-20 16:12:08 +0800

    1.0

--- pvetools.sh
+++ pvetools.sh
@@ -868,6 +868,7 @@ done
 main(){
 clear
 if [ $L = "en" ];then
+  echo -e "\033[32mVersion : 1.1\033[0m"
   echo -e "\033[32mPlease input to choose:\033[0m"
   echo -e "\033[32m[a] \033[31mGuide install.\033[0m"
   echo -e "\033[32m[b] \033[31mConfig apt source(change to ustc.edu.cn and so on).\033[0m"
@@ -885,6 +886,7 @@ if [ $L = "en" ];then
   echo -e "\033[32m[exit|q] Quit.\033[0m"
   echo -e "\033[32mInput:\033[0m"
 else
+  echo -e "\033[32mVersion : 1.1\033[0m"
   echo -e "\033[32m请输入序号选择相应的配置:\033[0m"
   echo -e "\033[32m[a] \033[31m无脑引导安装.\033[0m"
   echo -e "\033[32m[b] \033[31m配置apt源(更换为ustc.edu.cn,去除企业源等).\033[0m(100%)"
