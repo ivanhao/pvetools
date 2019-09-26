48f9f9f87c2cddfef9f1e98db188afd81e791e8d
ivanhao <ivanhao1984@qq.com>
2019-09-26 15:28:01 +0800

    1.2 chSensors to redo

--- pvetools.sh
+++ pvetools.sh
@@ -763,7 +763,6 @@ echo -e "\033[31mInstall lm-sensors and config web interface to display sensors 
 js='/usr/share/pve-manager/js/pvemanagerlib.js'
 pm='/usr/share/perl5/PVE/API2/Nodes.pm'
 sh='/usr/bin/s.sh'
-
 OS=`/usr/bin/pveversion|awk -F'-' 'NR==1{print $1}'`
 ver=`/usr/bin/pveversion|awk -F'/' 'NR==1{print $2}'|awk -F'-' '{print $1}'`
 bver=`/usr/bin/pveversion|awk -F'/' 'NR==1{print $2}'|awk -F'.' '{print $1}'`
@@ -786,20 +785,39 @@ if [[ -f "$js.backup" && -f "$sh" ]];then
     sleep 2
     main
 fi
-if [ ! -f "/usr/bin/sensors" ];then
-    echo "您还没有安装lm-sensors，将会自动进行安装配置："
-    echo "you have not installed lm-sensors, auto install now."
-    apt -y install lm-sensors
-    sensors-detect --auto > /tmp/sensors
-    for i in `sed -n '/Chip drivers/,/\#----cut here/p' /tmp/sensors|sed '/Chip /d'|sed '/cut/d'`;do modprobe $i;done
-    rm /tmp/sensors
-    sensors
-    echo -e "\033[31mInstall complete,if everything ok ,it's showed sensors.\033[0m"
-    echo -e "\033[31m安装配置成功，如果没有意外，上面已经显示sensors。\033[0m"
+case $L in
+    en )
+        echo -e "\033[32m[a] \033[31mInstall\033[0m"
+        echo -e "\033[32m[b] \033[31mUninstall\033[0m"
+        echo -e "\033[32m[q] \033[31mback to main menu.\033[0m"
+        ;;
+    zh )
+        echo -e "\033[32m[a] \033[31m安装\033[0m"
+        echo -e "\033[32m[b] \033[31m卸载\033[0m"
+        echo -e "\033[32m[q] \033[31m返回主菜单\033[0m"
+        ;;
+esac
+if [ $1 ];then
+    x=a
+else
+    read x
 fi
-sleep 2
-while [ true ]
-do
+case "$x" in
+    a )
+
+    if [ ! -f "/usr/bin/sensors" ];then
+        echo "您还没有安装lm-sensors，将会自动进行安装配置："
+        echo "you have not installed lm-sensors, auto install now."
+        apt -y install lm-sensors
+        sensors-detect --auto > /tmp/sensors
+        for i in `sed -n '/Chip drivers/,/\#----cut here/p' /tmp/sensors|sed '/Chip /d'|sed '/cut/d'`;do modprobe $i;done
+        rm /tmp/sensors
+        sensors
+        echo -e "\033[31mInstall complete,if everything ok ,it's showed sensors.\033[0m"
+        echo -e "\033[31m安装配置成功，如果没有意外，上面已经显示sensors。\033[0m"
+    fi
+    sleep 2
+
     echo "您的系统是：$pve, 您将安装sensors界面，是否继续？(y/n)"
     echo -n "Your OS：$pve, you will install sensors interface, continue?(y/n)"
     if [ `/usr/bin/pveversion|awk -F'/' 'NR==1{print $2}'|awk -F'.' '{print $1}'` != "5" ];then
@@ -886,8 +904,16 @@ EOF
         echo "Please input y/n to comfirm!"
         sleep 2
         chSensors
+    esac
+    ;;
+    b )
+        echo "b"
+        sleep 3
+        chSensors
+        ;;
+    q )
+        main
 esac
-done
 }
 #----------------------functions--end------------------#
 
