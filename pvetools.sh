6583987085aaf5c34ba5984808627fb644a6d8fa
ivanhao <ivanhao1984@qq.com>
2019-10-22 09:12:10 +0800

    2.0.1 add video & audio passthrough

--- pvetools.sh
+++ pvetools.sh
@@ -1596,7 +1596,8 @@ $(echo $cards) \
         for i in $DISTROS
         do
             echo $i >> id
-            ids=`lspci -n -s ${i}|awk '{print ","$3}'`
+            #ids=`lspci -n -s ${i}|awk '{print ","$3}'`
+            ids=`lspci -n -s $i`
             echo $ids >> ids
         done
         #whiptail --title "Success" --msgbox $ids 10 60
