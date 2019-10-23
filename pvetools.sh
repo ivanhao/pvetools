3f0dc2d419dfebcf0f26e44a760a2b6975bb45e0
ivanhao <ivanhao1984@qq.com>
2019-10-23 17:10:17 +0800

    2.0.1 add video & audio passthrough

--- pvetools.sh
+++ pvetools.sh
@@ -1708,8 +1708,11 @@ $(echo $cards) \
                 done
             done
             list=`qm list|awk 'NR>1{print $1":"$2".................."$3" "}'`
-            ls=`for i in $list;do echo $i|awk -F ":" '{print $1" "$2" OFF"}';done`
-            ls=`echo $ls|sed -n $confId 's/OFF/ON/p'`
+            echo -n "">lsvm
+            ls=`for i in $list;do echo $i|awk -F ":" '{print $1" "$2" OFF"}'>>lsvm;done`
+            ls=`sed -i '/'$confId'/ s/OFF/ON/g' lsvm` 
+            ls=`cat lsvm`
+            rm lsvm
             h=`echo $ls|wc -l`
             let h=$h*1
             if [ $h -lt 30 ];then
