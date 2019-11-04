4936f20127b6f8a519d7402caa338fa980524233
ivanhao <ivanhao1984@qq.com>
2019-11-04 14:34:00 +0800

    2.0.3 add qm set disk

--- pvetools.sh
+++ pvetools.sh
@@ -2371,7 +2371,7 @@ you choose: $vmid ,continue?
                     fi
                 done
                 if [ $1 = 'add' ];then
-                    disks=`ls -alh /dev/disk/by-id|awk '{print $11" "$9}'|awk -F "/" '{print $3}'|sed '/^$/d'|sed '/wwn/d'|sed '/^dm/d'|sed '/lvm/d'`
+                    disks=`ls -alh /dev/disk/by-id|awk '{print $11" "$9" OFF"}'|awk -F "/" '{print $3}'|sed '/^$/d'|sed '/wwn/d'|sed '/^dm/d'|sed '/lvm/d'`
                     d=$(whiptail --title " PveTools Version : 2.0.3 " --checklist "
 Choose disk:
 选择硬盘：" 20 60 10 \
